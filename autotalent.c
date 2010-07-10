8615d557a14afaf2cd9414f195f413672f142bb4
Ethan Chen <intervigil@gmail.com>
2010-07-10 03:43:40 -0700

    update license information to be GPL2 compliant

--- autotalent.c
+++ autotalent.c
@@ -6,6 +6,11 @@
    VERSION 0.2
    March 20, 2010
 
+   Modified by Ethan Chen
+   http://github.com/intervigilium/autotalent-harness
+   VERSION 0.1x
+   July 9, 2010
+
    This program is free software; you can redistribute it and/or modify        
    it under the terms of the GNU General Public License as published by        
    the Free Software Foundation; either version 2 of the License, or           
@@ -15,11 +20,6 @@
    but WITHOUT ANY WARRANTY; without even the implied warranty of              
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               
    GNU General Public License for more details.                                
-                                                                                
-   You should have received a copy of the GNU General Public License           
-   along with this program; if not, write to the Free Software                 
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  
-
  */
 
 /*****************************************************************************/
